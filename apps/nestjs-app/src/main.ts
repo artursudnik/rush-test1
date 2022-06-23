44b6c66b38baea22b54d39d3b32c14d463615b6d
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2022-06-23 12:04:18 +0200

    implementing swagger endpoint

--- apps/nestjs-app/src/main.ts
+++ apps/nestjs-app/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwaggerDocument(app);
   await app.listen(3000);
 }
 bootstrap();
+
+
+function setupSwaggerDocument(app: INestApplication): OpenAPIObject {
+  const config = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  return document;
+}
